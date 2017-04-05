f689584d0e58fa5e42446610a273d381b9a631aa
super-will <593582509@qq.com>
2017-04-05 22:07:57 +0800

    Add qrcode parser to parse qrcode image
    Commit by will

---
+++ src/main/java/com/superwill/qrcode/QRcodeParser.java
@@ -0,0 +1,46 @@
+package com.superwill.qrcode;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+import com.alibaba.fastjson.JSONObject;
+import com.google.zxing.Binarizer;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+
+/**
+ * 解析二维码
+ * @author hcguo
+ *
+ */
+public class QRcodeParser {
+    
+    public static String parse(String path){
+    	String content = "";
+         try {
+			BufferedImage image = ImageIO.read(new File(path));
+			LuminanceSource source = new BufferedImageLuminanceSource(image);
+			Binarizer binarizer = new HybridBinarizer(source);
+			BinaryBitmap binaryBitmap = new BinaryBitmap(binarizer);
+			Map<DecodeHintType, Object> hints = new HashMap<DecodeHintType,Object>();
+			hints.put(DecodeHintType.CHARACTER_SET, "UTF-8");
+			Result result = new MultiFormatReader().decode(binaryBitmap, hints);
+			content = result.getText();
+            System.out.println("解析内容为："+result.getText());
+            
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+         return content;
+    }
+}
